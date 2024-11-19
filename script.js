f59ec8f7077a0b37dbf752f17e728143054f5850
Jenaya F <145370280+Shava-mp4@users.noreply.github.com>
2024-11-19 10:18:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,60 @@
 //Snowflakes
 let snowflakesArray = [];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 250; i++) {
   snowflakesArray.push({
     x: Math.random() * cnv.width,
     y: 0,
-    r: Math.random() * 5 + 1,
-    dx: Math.random() * 6 + 1, // 2.01 - 5.99
+    r: Math.random() * 2 + 1,
+    dx: Math.random() * (1.2 - 0.2) + 0.2, // 2.01 - 5.99
+    dy: Math.random() * (2 - 0.5) + 0.5,
+    speed: 1,
   });
 }
 
 console.log(snowflakesArray);
+
+// // Key events
+document.addEventListener("keydown", keydownHandler);
+
+// --------Add/Remove 10 Snowflakes------------
+function keydownHandler(event) {
+  console.log(event.code);
+
+  if (event.code == "ArrowRight") {
+    for (let i = 0; i < 10; i++) {
+      snowflakesArray.push({
+        x: Math.random() * cnv.width,
+        y: 0,
+        r: Math.random() * 2 + 1,
+        dx: Math.random() * (1.2 - 0.2) + 0.2, // 2.01 - 5.99
+        dy: Math.random() * (2 - 0.5) + 0.5,
+        speed: 1,
+      });
+    }
+  }
+
+  if (event.code == "ArrowLeft") {
+    for (let i = 0; i < 10; i++) {
+      snowflakesArray.pop();
+    }
+  }
+
+  if (event.code == "ArrowUp") {
+    for (let i = 0; i < snowflakesArray.length; i++) {
+      snowflakesArray[i].speed + 0.5;
+      if (snowflakesArray[i].speed > 10) {
+        snowflakesArray[i].speed = 10;
+      }
+    }
+  }
+
+  if (event.code == "ArrowDown") {
+    for (let i = 0; i < snowflakesArray.length; i++) {
+      snowflakesArray[i].speed - 0.2;
+      if (snowflakesArray[i].speed < 0.2) {
+        snowflakesArray[i].speed = 0.2;
+      }
+    }
+  }
+}
