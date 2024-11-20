4b329243ac0c671b40b6dd36009d64ff804d8d6c
Jenaya F <145370280+Shava-mp4@users.noreply.github.com>
2024-11-20 10:17:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,55 @@
 
 //Snowflakes
 let snowflakesArray = [];
+let snowmanArray = [
+  //Default
+  {
+    armWidth: lineWidth(5),
+    rightArm: line(780, 420, 760, 480),
+    colourOne: (ctx.fillStyle = "rgb(230, 231, 240)"),
+    torso: circle(810, 440, 43, "fill"),
+    colourTwo: (ctx.fillStyle = "rgb(28, 27, 31)"),
+    buttonOne: circle(798, 417, 3, "fill"),
+    buttonTwo: circle(794, 437, 3, "fill"),
+    buttonThree: circle(797, 455, 3, "fill"),
+    leftArm: line(840, 420, 860, 480),
+  },
+
+  //pose1
+  {
+    armWidth: lineWidth(5),
+    rightArm: line(780, 420, 760, 360),
+    colourOne: (ctx.fillStyle = "rgb(230, 231, 240)"),
+    torso: circle(810, 440, 43, "fill"),
+    colourTwo: (ctx.fillStyle = "rgb(28, 27, 31)"),
+    buttonOne: circle(798, 417, 3, "fill"),
+    buttonTwo: circle(794, 437, 3, "fill"),
+    buttonThree: circle(797, 455, 3, "fill"),
+    leftArm: line(840, 420, 860, 360),
+  },
+
+  //pose2
+  {
+    armWidth: lineWidth(5),
+    rightArm: line(780, 420, 760, 360),
+    colourOne: (ctx.fillStyle = "rgb(230, 231, 240)"),
+    torso: circle(810, 440, 43, "fill"),
+    colourTwo: (ctx.fillStyle = "rgb(28, 27, 31)"),
+    buttonOne: circle(798, 417, 3, "fill"),
+    buttonTwo: circle(794, 437, 3, "fill"),
+    buttonThree: circle(797, 455, 3, "fill"),
+    leftArm1: line(840, 420, 870, 430),
+    leftArm2: line(868, 430, 875, 390),
+  },
+];
 
 for (let i = 0; i < 250; i++) {
   snowflakesArray.push({
-    x: Math.random() * cnv.width,
+    x: Math.random() * 1000 - 100,
     y: 0,
     r: Math.random() * 2 + 1,
     dx: Math.random() * (1.2 - 0.2) + 0.2, // 2.01 - 5.99
     dy: Math.random() * (2 - 0.5) + 0.5,
-    speed: 1,
   });
 }
 
@@ -31,7 +71,6 @@ function keydownHandler(event) {
         r: Math.random() * 2 + 1,
         dx: Math.random() * (1.2 - 0.2) + 0.2, // 2.01 - 5.99
         dy: Math.random() * (2 - 0.5) + 0.5,
-        speed: 1,
       });
     }
   }
@@ -44,19 +83,27 @@ function keydownHandler(event) {
 
   if (event.code == "ArrowUp") {
     for (let i = 0; i < snowflakesArray.length; i++) {
-      snowflakesArray[i].speed + 0.5;
-      if (snowflakesArray[i].speed > 10) {
-        snowflakesArray[i].speed = 10;
+      if (snowflakesArray[i].dx < 10) {
+        snowflakesArray[i].dx + 0.5;
       }
     }
   }
 
   if (event.code == "ArrowDown") {
     for (let i = 0; i < snowflakesArray.length; i++) {
-      snowflakesArray[i].speed - 0.2;
-      if (snowflakesArray[i].speed < 0.2) {
-        snowflakesArray[i].speed = 0.2;
+      if (snowflakesArray[i].dx > 0.2) {
+        snowflakesArray[i].dx - 0.2;
       }
     }
   }
+
+  if (event.code == "KeyP") {
+    let pose = 0;
+    pose++;
+    snowmanArray[pose];
+    if (pose == snowmanArray.length) {
+      pose = 0;
+    }
+  }
 }
+
